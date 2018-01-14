9c6823d7730a7a1c68dd5f87b9affbc2a3387acb
Andrea Bertagnolli <andrea.bertagnolli@gmail.com>
2018-01-14 14:16:32 +0100

    Handle media absence and try with different urls

--- download-rollins.py
+++ download-rollins.py
@@ -2,17 +2,26 @@ import sys
 from sys import argv
 import requests
 import json
-import os.path
+import os
 import datetime
 
 def download(url):
     filename = url.split('/')[-1]
     if (os.path.isfile(filename)):
         print ("File {} already downloaded".format(filename))
+        return False
     else:
         print ("Download from {}".format(url))
         with open(filename, "wb") as file:
             file.write(requests.get(url).content)
+
+        if os.stat(filename).st_size == 0:
+            print ("Empty file, remove")
+            os.remove(filename)
+            return False
+        else:
+            print ("File {} downloaded correctly".format(filename))
+            return True
 
 def toShortDate(iso8601datetime):
     parsed = datetime.datetime.strptime(iso8601datetime[:19], '%Y-%m-%dT%H:%M:%S')
@@ -32,11 +41,12 @@ else:
             if (len(media) == 0):
                 print ("Media not available for show #{} trying to download in other way...".format(number))
                 shortdate = toShortDate(parsed['airdate'])
-                newUrl = "https://kcrw-od.streamguys1.com/kcrw/audio/website/music/hr/hr{}kcrw_broadcast_{}.mp3".format(shortdate, number)
-                print ("new url", newUrl)
-                download(newUrl)
+                download("https://podcast-download.kcrw.com/kcrw/audio/podcast/music/hr/hr{}KCRW_Broadcast_{}.mp3".format(shortdate, number))
+                download("https://kcrw-od.streamguys1.com/kcrw/audio/website/music/hr/hr{}kcrw_broadcast_{}.mp3".format(shortdate, number))
+                download("https://kcrw-od.streamguys1.com/kcrw/audio/website/music/hr/KCRW-henry_rollins-kcrw_broadcast_{}.mp3".format(number))
             else:
                 print ("Media available for show #{}".format(number))
                 download(media[0]['url'])
         else:
             print ("Not a json")
+            download("https://kcrw-od.streamguys1.com/kcrw/audio/website/music/hr/KCRW-henry_rollins-kcrw_broadcast_{}.mp3".format(number))
